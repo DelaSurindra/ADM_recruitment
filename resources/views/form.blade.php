435dd2dcb0915f0b0a43a4dd42ae3f28797f750f
edubro <eduardus.adijala@gmail.com>
2020-02-03 13:57:52 +0700

    update code facebook pixel:

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -24,7 +24,15 @@
     <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
     <link href="{{asset('assets/css/themify-icons.css')}}" rel="stylesheet">
     <link href="{{asset('assets/css/datepicker.min.css')}}" rel="stylesheet">
+    
+    <!-- Facebook Pixel Code -->
+    <script>
+      !function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '2648797392022220'); fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2648797392022220&ev=PageView&noscript=1" /></noscript>
+    <!-- End Facebook Pixel Code -->
 
+</head>
     <body>
       @if(Session::has("error_msg"))
           <input type="hidden" name="error_msg" value="{{Session::get('error_msg')}}">
