7385fd0c8ace3fd5f08f15577de442a34748721f
Mohammad Wahyu Kurniawan <wahyukur81@gmail.com>
2021-02-17 16:28:21 +0700

    remove dummy list

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -41,17 +41,17 @@ class VacancyController extends Controller
             $listVacancy = $listVacancy->orderBy('created_at', $dataSend["sort"])->get()->toArray();
         }
         // DUMMY
-        $listVacancy = [
-            [
-                'job_poster' => 'https://images.unsplash.com/photo-1597404294360-feeeda04612e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                'job_title' => 1,
-                'id' => 1,
-            ], [
-                'job_poster' => 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                'job_title' => 1,
-                'id' => 2,
-            ],
-        ];
+        // $listVacancy = [
+        //     [
+        //         'job_poster' => 'https://images.unsplash.com/photo-1597404294360-feeeda04612e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        //         'job_title' => 1,
+        //         'id' => 1,
+        //     ], [
+        //         'job_poster' => 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        //         'job_title' => 1,
+        //         'id' => 2,
+        //     ],
+        // ];
         
         if ($listVacancy != null) {
             $response = array(
