b99255369cdd10305fe28a9cbfb4b28d8e90af85
Bagus Fibrianto <bfibrianto@gmail.com>
2021-04-15 11:06:40 +0700

    penyesuaian scoring invetory dan api get time

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -65,5 +66,14 @@ class Handler extends ExceptionHandler
         }
         return parent::render($request, $exception);
 
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['code'=>401,'message'=> 'Invalid authorization token.'], 401);
+        }
+
+        return redirect()->guest(route('login'));
     }
 }
