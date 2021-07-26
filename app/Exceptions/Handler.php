e93512db0c412c8daa5f586d7169c818ea4aef2f
Mohammad Wahyu Kurniawan <wahyukur81@gmail.com>
2021-07-26 17:05:52 +0700

    fix merge

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,9 +47,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        // if ($exception->getMessage() != "" || $exception->getMessage() != null) {
-        //     dd($exception, $request);
-        // }
         // if ($exception instanceof \Symfony\Component\HttpFoundation\File\Exception\FileException) {
         //     // create a validator and validate to throw a new ValidationException
         //     // return Validator::make($request->all(), [
