8c9d3a10cd29db233959e2101f04c60553579fac
Mohammad Wahyu Kurniawan <wahyukur81@gmail.com>
2021-07-26 17:04:52 +0700

    validation excel bulk add candidate

--- app/Imports/addBulkCandidate.php
+++ app/Imports/addBulkCandidate.php
@@ -9,6 +9,7 @@ use App\Model\User;
 use App\Model\Education;
 use App\Jobs\JobSendEmail;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Facades\Validator;
 
 class addBulkCandidate implements ToCollection
 {
@@ -20,6 +21,14 @@ class addBulkCandidate implements ToCollection
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    private $messages = array(
+		"required" => "Mohon isi kolom ini.",
+        "regex" => "Format tidak sesuai"
+	);
+
+    private $label = array(
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
+    );
 
     public function __construct()
     {
@@ -32,82 +41,121 @@ class addBulkCandidate implements ToCollection
         if (count($collection) == 1) {
             // $this->dataBulk($this->data);
         }else{
-            for ($i=1; $i < count($collection); $i++) {
-                $checkUser = User::where('email', $collection[$i][2])->get()->toArray();
-                if ($checkUser) {
-                    $dataRes = [
-                        'email' => $collection[$i][2],
-                        'status' => 'Failed',
-                        'note' => 'Email already exist'
-                    ];
-                    array_push($this->data, $dataRes);
-                } else {
-                    $password = $this->generateRandomString(8);
-                    $insertUser = User::insertGetId([
-                        'email' => $collection[$i][2],
-                        'type' => '1',
-                        'password' => bcrypt($password.env('SALT_PASS_CANDIDATE'))
-                    ]);
-    
-                    if ($collection[$i][4] == "male" || $collection[$i][4] == "Male" || $collection[$i][4] == "MALE") {
-                        $gender = "1";
-                    } else {
-                        $gender = "2";
-                    }
-    
-                    if($collection[$i][8] == "d3" || $collection[$i][8] == "D3"){
-                        $gelar = "1";
-                    }elseif ($collection[$i][8] == "s1" || $collection[$i][8] == "S1") {
-                        $gelar = "2";
-                    }else{
-                        $gelar = "3";
-                    }
-                    
-                    $candidate = Candidate::insertGetId([
-                        'first_name' => $collection[$i][0],
-                        'last_name' => $collection[$i][1],
-                        'tanggal_lahir' => date('Y-m-d', strtotime($collection[$i][3])),
-                        'gender' => $gender,
-                        'telp' => $collection[$i][5],
-                        'kota' => $collection[$i][6],
-                        'linkedin' => "",
-                        'cover_letter' => "",
-                        'resume' => "",
-                        'protofolio' => "",
-                        'foto_profil' => "",
-                        'skill' => "",
-                        'user_id' => $insertUser,
-                        'status' => 0
-                    ]);
-                    $education = Education::insert([
-                        "universitas" => $collection[$i][7],
-                        "gelar" => $gelar,
-                        "fakultas" => $collection[$i][9],
-                        "jurusan" => $collection[$i][10],
-                        "start_year" => $collection[$i][11],
-                        "end_year" => $collection[$i][12],
-                        "gpa" => str_replace(',', '.', round($collection[$i][13], 2)),
-                        "kandidat_id" => $candidate,
-                    ]);
-                    $dataRes = [
-                        'email' => $collection[$i][2],
-                        'status' => 'Success',
-                        'note' => ''
-                    ];
-                    array_push($this->data, $dataRes);
-                    $dataEmail = [
-                        'email'         => $collection[$i][2],
-                        'nama'          => $collection[$i][0].' '.$collection[$i][1],
-                        'password'      => $password,
-                        'subject'       => 'Register Invitation',
-                        'view'          => 'email.email-invit-candidate'
-                    ];
-                    
-                    $response = JobSendEmail::dispatch($dataEmail);
-                }
+            $rows = $collection->toArray();
+            $header = $rows[0];
+            unset($rows[0]);
+            $rows = array_values($rows);
+            // dd($rows);
+            try {
+                Validator::make($rows, [
+                    '*.0' => ["required", "regex:/^[a-zA-Z.' ]*$/"], // first name
+                    '*.1' => ["required", "regex:/^[a-zA-Z.' ]*$/"], // last name
+                    '*.2' => ["required"], // email
+                    '*.3' => ["required"], // birth date
+                    '*.4' => ["required"], // gender
+                    '*.5' => ["required", "regex:/^\d{11,13}$/"], // phone
+                    '*.6' => ["required"], // location
+                    '*.7' => ["required", "regex:/^[a-zA-Z0-9 ]*$/"], // univ
+                    '*.8' => ["required"], // degree
+                    '*.9' => ["required", "regex:/^[a-zA-Z0-9 ]*$/"], // faculty
+                    '*.10' => ["required"], // major
+                    '*.11' => ["required"], // start year
+                    '*.12' => ["required"], // end year
+                    '*.13' => ["required"], // gpa
+                ], $this->messages)->validate();
                 
+                for ($i=0; $i < count($rows); $i++) { 
+                    $checkUser = User::where('email', $rows[$i][2])->get()->toArray();
+                    if ($checkUser) {
+                        $dataRes = [
+                            'email' => $rows[$i][2],
+                            'status' => 'Failed',
+                            'note' => 'Email already exist'
+                        ];
+                        array_push($this->data, $dataRes);
+                    } else {
+                        $password = $this->generateRandomString(8);
+                        $insertUser = User::insertGetId([
+                            'email' => $rows[$i][2],
+                            'type' => '1',
+                            'password' => bcrypt($password.env('SALT_PASS_CANDIDATE'))
+                        ]);
+                        
+                        if ($rows[$i][4] == "male" || $rows[$i][4] == "Male" || $rows[$i][4] == "MALE") {
+                            $gender = "1";
+                        } else {
+                            $gender = "2";
+                        }
+    
+                        if($rows[$i][8] == "d3" || $rows[$i][8] == "D3"){
+                            $gelar = "1";
+                        }elseif ($rows[$i][8] == "s1" || $rows[$i][8] == "S1") {
+                            $gelar = "2";
+                        }else{
+                            $gelar = "3";
+                        }
+
+                        $candidate = Candidate::insertGetId([
+                            'first_name' => $rows[$i][0],
+                            'last_name' => $rows[$i][1],
+                            'tanggal_lahir' => date('Y-m-d', strtotime($rows[$i][3])),
+                            'gender' => $gender,
+                            'telp' => $rows[$i][5],
+                            'kota' => $rows[$i][6],
+                            'linkedin' => "",
+                            'cover_letter' => "",
+                            'resume' => "",
+                            'protofolio' => "",
+                            'foto_profil' => "",
+                            'skill' => "",
+                            'user_id' => $insertUser,
+                            'status' => 0
+                        ]);
+                        $education = Education::insert([
+                            "universitas" => $rows[$i][7],
+                            "gelar" => $gelar,
+                            "fakultas" => $rows[$i][9],
+                            "jurusan" => $rows[$i][10],
+                            "start_year" => $rows[$i][11],
+                            "end_year" => $rows[$i][12],
+                            "gpa" => str_replace(',', '.', round($rows[$i][13], 2)),
+                            "kandidat_id" => $candidate,
+                        ]);
+                        $dataRes = [
+                            'email' => $rows[$i][2],
+                            'status' => 'Success',
+                            'note' => ''
+                        ];
+                        array_push($this->data, $dataRes);
+                        $dataEmail = [
+                            'email'         => $rows[$i][2],
+                            'nama'          => $rows[$i][0].' '.$rows[$i][1],
+                            'password'      => $password,
+                            'subject'       => 'Register Invitation',
+                            'view'          => 'email.email-invit-candidate'
+                        ];
+                        
+                        $response = JobSendEmail::dispatch($dataEmail);
+                    }
+                }
+            } catch (\Throwable $th) {
+                // dd($th);
+                $error = $th->validator->failed();
+                $message = $th->validator->messages()->messages();
+                $data = $th->validator->getData();
+                // dd($error, $message, $data);
+                $key = array_key_first($error);
+                $pecah = explode('.', $key);
+                $val = array_values($message)[0];
+                
+                $dataRes = [
+                    'email' => $data[$pecah[0]][$pecah[1]],
+                    'status' => 'Failed',
+                    'note' => 'Line '.$this->label[$pecah[1]].($pecah[0]+2).' : '.$val[0]
+                ];
+                // dd($dataRes);
+                array_push($this->data, $dataRes);
             }
-            // $this->dataBulk($this->data);
         }
     }
 
